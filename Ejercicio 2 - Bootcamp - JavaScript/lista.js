aa26012f6cee2e44dab0d09556ef372066ea9701
juanmasorca <juanmasorca@gmail.com>
2023-04-12 10:58:27 -0500

    update

---
+++ Ejercicio 2 - Bootcamp - JavaScript/lista.js
@@ -0,0 +1,16 @@
+//declarar una lista como constante
+const miInformacion = {
+  nombre: "Juan",
+  edad: 24,
+  Desarrollador: true,
+  fechaNacimiento: new Date("1998-08-22"),
+  libroFavorito: {
+    titulo: "La bruja",
+    autor: "Germán Castro Caycedo",
+    fecha: new Date("1985-01-22"),
+    url: "https://www.planetadelibros.com.co/libro-la-bruja/162087"
+  }
+};
+
+//mostrar por consola la informacion
+console.log(miInformacion);
